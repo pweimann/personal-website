7d8a0cb97184da61085093cb74e60974f19f77cd
paul.weimann <paul.weimann@ista-express.de>
2022-09-16 21:01:48 +0200

    adjust the global font styles

--- app.vue
+++ app.vue
@@ -13,27 +13,36 @@
 import BaseFooter from '@/components/BaseFooter'
 import BaseHeader from '@/components/BaseHeader'</script>
 <style lang='css'>
+html {
+  font-size: 10px;
+  font-family: Roboto, sans-serif;
+}
+
 * {
   margin: 0;
   padding: 0;
-  font-size: 10px;
-  font-family: Roboto, sans-serif;
   box-sizing: border-box;
 }
 
 p {
-  font-size: 1.8rem;
+  font-size: 1.6rem;
 }
 
 h1 {
-  font-size: 7rem;
+  font-size: calc(7rem + 1vw);
 }
 
 h2 {
-  font-size: 3.5rem;
+  font-size: calc(3rem + 1vw);
 }
 
 p {
   margin-top: 1rem;
+}
+
+@media screen and (min-width: 800px) {
+  p {
+    font-size: 1.8rem;
+  }
 }
 </style>
