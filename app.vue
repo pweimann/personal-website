a162f942f2d0ef06476ccf0321aab38b77a18ebc
paul.weimann <paul.weimann@ista-express.de>
2022-09-16 19:07:35 +0200

    implement basic page structure and adds the index page

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <template>
   <div>
+    <BaseHeader></BaseHeader>
+    <NuxtPage />
+    <BaseFooter></BaseFooter>
   </div>
 </template>
+<script lang='ts' setup>
+import BaseFooter from '@/components/BaseFooter'
+import BaseHeader from '@/components/BaseHeader'</script>
---
+++ components/BaseHeader.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+  </header>
+</template>
+<script lang='ts'></script>
+<style lang='css'>
+</style>
