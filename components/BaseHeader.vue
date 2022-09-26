22b17a236cc4e2d79179cec5c0b052e654ee91d4
paul.weimann <paul.weimann@ista-express.de>
2022-09-26 17:23:06 +0200

    add github link

--- components/BaseHeader.vue
+++ components/BaseHeader.vue
@@ -1,14 +1,17 @@
 <template>
   <header>
-    <BaseHeaderItem title='Contact' class='ml-auto'>
-      <a href='https://twitter.com/paul__weimann'>
-        <img src='/icon-awesome-twitter.svg' alt='twitter' />
+    <BaseHeaderItem title="Contact" class="ml-auto">
+      <a href="https://twitter.com/paul__weimann">
+        <img src="/icon-awesome-twitter.svg" alt="twitter">
       </a>
-      <a href='https://www.instagram.com/bluubsen/'>
-        <img src='/icon-awesome-instagram.svg' alt='instagram' />
+      <a href="https://github.com/pweimann">
+        <img src="/icon-awesome-github.svg" alt="github">
       </a>
-      <a href='https://www.linkedin.com/in/paul-weimann-7b9852207/'>
-        <img src='/icon-awesome-linkedin.svg' alt='linkedin' />
+      <a href="https://www.instagram.com/bluubsen/">
+        <img src="/icon-awesome-instagram.svg" alt="instagram">
+      </a>
+      <a href="https://www.linkedin.com/in/paul-weimann-7b9852207/">
+        <img src="/icon-awesome-linkedin.svg" alt="linkedin">
       </a>
     </BaseHeaderItem>
   </header>
