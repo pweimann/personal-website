461552f76d92c6b4c5e7fc3fcd81149fea706965
paul.weimann <paul.weimann@ista-express.de>
2022-09-28 15:10:40 +0200

    update the html head with some meta data

--- app.vue
+++ app.vue
@@ -10,8 +10,24 @@
   </NuxtLayout>
 </template>
 <script lang='ts' setup>
+import { useHead } from '#head'
 import BaseFooter from '@/components/BaseFooter'
-import BaseHeader from '@/components/BaseHeader'</script>
+import BaseHeader from '@/components/BaseHeader'
+
+useHead({
+  title: 'Personal website of Paul Weimann',
+  meta: [
+    {
+      name: 'description',
+      content: 'I am a frontend developer with a love for pixels, currently based in Berlin. ' +
+        'This personal website was created to share my work, my journey as a developer, and my personal development.'
+    }
+  ],
+  htmlAttrs: {
+    lang: 'en'
+  }
+})
+</script>
 <style lang='css'>
 html {
   font-size: calc(10px + 0.15vw);
