04a0f01ac05d31600577443beac31d37daa68a3c
paul.weimann <paul.weimann@ista-express.de>
2022-10-27 17:33:13 +0100

    commit initial burger menu component

--- components/BurgerMenu.vue
+++ components/BurgerMenu.vue
@@ -1,4 +1,31 @@
-<template lang="html">
-  <div />
+<template>
+  <div class="menu-wrapper">
+    <div class="menu-btn">
+      <div class="line" />
+      <div class="line" />
+    </div>
+  </div>
 </template>
 <script setup lang='ts'></script>
+<style lang='css'>
+.menu-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+}
+
+.menu-btn {
+  cursor: pointer;
+  padding: 2rem;
+}
+
+.line {
+  background-color: black;
+  height: 0.2rem;
+  width: 4rem;
+}
+
+.line + .line {
+  margin-top: 0.5rem;
+}
+</style>
