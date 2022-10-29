c07eda642019feff9b45428ea72ad37ba0700a83
paul.weimann <paul.weimann@ista-express.de>
2022-10-29 19:00:48 +0100

    implement the burger menu and the full overflow menu

--- components/BurgerMenu.vue
+++ components/BurgerMenu.vue
@@ -1,25 +1,56 @@
 <template>
-  <div class="menu-wrapper">
+  <div class="menu-wrapper" :class="menuWrapperClasses" @click="onClick">
     <div class="menu-btn">
       <div class="line" />
       <div class="line" />
     </div>
   </div>
 </template>
-<script setup lang='ts'></script>
-<style lang='css'>
+<script setup lang='ts'>
+import { computed, ref } from '#imports'
+
+const isMenuToggled = ref(false)
+
+const onClick = () => {
+  isMenuToggled.value = !isMenuToggled.value
+}
+
+const menuWrapperClasses = computed(() => {
+  return isMenuToggled.value ? 'active-menu' : ''
+})
+</script>
+<style scoped lang='css'>
 .menu-wrapper {
   display: flex;
   flex-direction: column;
   justify-content: start;
 }
 
+.active-menu .line {
+  transition: all 0.5s ease;
+  background-color: #262626;
+}
+
+.active-menu .menu-btn:hover .line {
+  background-color: white;
+}
+
+.active-menu .line:nth-of-type(1) {
+  transform: rotate(45deg) translate(0px, 6px);
+}
+
+.active-menu .line:nth-of-type(2) {
+  transform: rotate(135deg) translate(0px, 6px);
+}
+
 .menu-btn {
+  position: relative;
   cursor: pointer;
-  padding: 2rem;
+  padding: 4rem 2rem;
 }
 
 .line {
+  transition: all 0.5s ease;
   background-color: black;
   height: 0.2rem;
   width: 4rem;
@@ -27,5 +58,11 @@
 
 .line + .line {
   margin-top: 0.5rem;
+}
+
+.line:nth-of-type(2) {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ pages/projects.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="projects" />
+</template>
+<style scoped lang='css'>
+</style>
