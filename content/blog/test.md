e9bc877f51f0ba4e26486459ab2d73125dc685b4
paul.weimann <paul.weimann@ista-express.de>
2022-11-09 14:01:09 +0100

    Implement an overview of all blog articles.

--- content/blog/test.md
+++ content/blog/test.md
@@ -1,1 +1,7 @@
+---
+navigation:
+title: 'Test Content'
+description: 'Adapt what is useful, reject what is useless, and add what is specifically your own.'
+---
+
 # Hello Content
--- pages/blog.vue
+++ pages/blog.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
     <h1>Blog</h1>
-    <div />
+    <ContentList v-slot="{ list }" :query="query">
+      <NuxtLink v-for="blog in list" :key="blog._path" :to="blog._path">
+        <h2>{{ blog.title }}</h2>
+        <p>{{ blog.description }}</p>
+      </NuxtLink>
+    </ContentList>
   </div>
 </template>
+<script setup lang='ts'>
+import { QueryBuilderParams } from '@nuxt/content/dist/runtime/types'
+
+const query: QueryBuilderParams = { path: '/blog', limit: 5 }
+</script>
 <style scoped lang='css'>
 </style>
